b6b1ea3db368021464fa779fbc05fce89c97f54f
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-03-14 01:58:00 -0300

    feat: Exporting all components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+import { Cards, Chart, CountryPicker } from "./components";
+
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     )
   }
