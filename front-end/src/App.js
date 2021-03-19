3c2f4ecb9904fb34e10d4a967543c9b780ce2031
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-03-18 22:04:30 -0300

    feat: Cards statistic

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,26 @@ import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from './App.module.css';
+import { fetchData} from './api';
 
 class App extends React.Component {
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+
+
   render() {
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data}/>
         <CountryPicker />
         <Chart />
       </div>
